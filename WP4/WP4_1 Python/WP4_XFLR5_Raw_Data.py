11f923db6d3a2e1add9711122a7d34a0bfc25553
nachos-dighe <ndighe2002@gmail.com>
2021-11-10 15:10:03 +0100

    Update WP4_XFLR5_Raw_Data.py

--- WP4/WP4_1 Python/WP4_XFLR5_Raw_Data.py
+++ WP4/WP4_1 Python/WP4_XFLR5_Raw_Data.py
@@ -3,26 +3,26 @@ import matplotlib.pyplot as plt
 import scipy as sp
 from scipy import interpolate
 
+#import XFLR5 data for AOA 0 and 10 deg
 aero_data_AOA_0 = np.genfromtxt('MainWing_a=0.00_v=10.00ms.txt', dtype = float, skip_header=2)
 aero_data_AOA_10 = np.genfromtxt('MainWing_a=10.00_v=10.00ms.txt', dtype = float, skip_header=2)
 
-'''
 #testing works
+'''
 print(aero_data_AOA_0)
 Cdlst = aero_data_AOA_0[:, 5]
 print(Cdlst)
 '''
 
+#aerodynamics coefficients (non-dimensional)
 def aero_coefficient(aero_data_AOA):
+    xlst = aero_data_AOA[:,1]
     ylst = aero_data_AOA[:,0]
     Cllst = aero_data_AOA[:,3]
     Cdlst = aero_data_AOA[:, 5]
+    print(Cdlst)
     Cmlst = aero_data_AOA[:, 7] #pitching moment about c/4 point
-    return(ylst,Cllst, Cdlst, Cmlst)
-
-ylst_0,Cllst_0, Cdlst_0, Cmlst_0 = aero_coefficient(aero_data_AOA_0)
-ylst_10,Cllst_10, Cdlst_10, Cmlst_10 = aero_coefficient(aero_data_AOA_10)
-
+    return(xlst,ylst,Cllst, Cdlst, Cmlst)
 
 #aerodynamics loads (dimensional)
 def aero_loads(xlst, ylst,Cllst, Cdlst, Cmlst):
@@ -39,10 +39,19 @@ def aero_loads(xlst, ylst,Cllst, Cdlst, Cmlst):
     Mtot = np.sum(Mlst)
     return(Llst,Dlst,Mlst, Ltot, Dtot, Mtot)
 
-    Llst_0,Dlst_0,Mlst_0, Ltot_0, Dtot_0, Mtot_0 = aero_loads(xlst_0, ylst_0,Cllst_0, Cdlst_0, Cmlst_0)
-    Llst_10,Dlst_10,Mlst_10, Ltot_10, Dtot_10, Mtot_10 = aero_loads(xlst_10, ylst_10,Cllst_10, Cdlst_10, Cmlst_10)
+
+#lists for aerodynamic coefficients
+xlst_0,ylst_0,Cllst_0, Cdlst_0, Cmlst_0 = aero_coefficient(aero_data_AOA_0)
+xlst_10,ylst_10,Cllst_10, Cdlst_10, Cmlst_10 = aero_coefficient(aero_data_AOA_10)
+
+#lists for aerodynamic loads
+Llst_0,Dlst_0,Mlst_0, Ltot_0, Dtot_0, Mtot_0 = aero_loads(xlst_0, ylst_0,Cllst_0, Cdlst_0, Cmlst_0)
+Llst_10,Dlst_10,Mlst_10, Ltot_10, Dtot_10, Mtot_10 = aero_loads(xlst_10, ylst_10,Cllst_10, Cdlst_10, Cmlst_10)
 
 
 
-
+#interpolation
+Cl_interp0 = sp.interpolate.interp1d(ylst_0,Cllst_0, kind = "cubic", fill_value="extrapolate")
+Cd_interp0 = sp.interpolate.interp1d(ylst_0,Cdlst_0, kind = "cubic", fill_value="extrapolate")
+Cm_interp0 = sp.interpolate.interp1d(ylst_0,Cmlst_0, kind = "cubic", fill_value="extrapolate")
 
