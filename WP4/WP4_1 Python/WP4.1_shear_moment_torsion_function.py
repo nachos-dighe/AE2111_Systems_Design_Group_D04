c6dee738380a7d85fc66d80e30b2560ac261b796
BillieSeffer <b.r.n.seffer@student.tudelft.nl>
2021-11-10 17:30:06 +0100

    cleaning files

--- WP4/WP4_1 Python/WP4.1_shear_moment_torsion_function.py
+++ WP4/WP4_1 Python/WP4.1_shear_moment_torsion_function.py
@@ -4,7 +4,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy as sp
 from scipy import interpolate
-from scipy import integrate 
+from scipy import integrate
+
+def y (x) :
+    return x ** 3 
+
+print(sp.integrate.quad(y,0,12))
 
 
 
