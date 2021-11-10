38457b4918a9f5169d8655bad3db70c8cc34b29b
nachos-dighe <ndighe2002@gmail.com>
2021-11-10 14:44:19 +0100

    Update WP4_XFLR5_Raw_Data.py

--- WP4/WP4_1 Python/WP4_XFLR5_Raw_Data.py
+++ WP4/WP4_1 Python/WP4_XFLR5_Raw_Data.py
@@ -6,17 +6,17 @@ from scipy import interpolate
 aero_data_AOA_0 = np.genfromtxt('MainWing_a=0.00_v=10.00ms.txt', dtype = float, skip_header=2)
 aero_data_AOA_10 = np.genfromtxt('MainWing_a=10.00_v=10.00ms.txt', dtype = float, skip_header=2)
 
+'''
 #testing works
 print(aero_data_AOA_0)
 Cdlst = aero_data_AOA_0[:, 5]
 print(Cdlst)
-
+'''
 
 def aero_coefficient(aero_data_AOA):
     ylst = aero_data_AOA[:,0]
     Cllst = aero_data_AOA[:,3]
     Cdlst = aero_data_AOA[:, 5]
-    print(Cdlst)
     Cmlst = aero_data_AOA[:, 7] #pitching moment about c/4 point
     return(ylst,Cllst, Cdlst, Cmlst)
 
@@ -39,10 +39,10 @@ def aero_loads(xlst, ylst,Cllst, Cdlst, Cmlst):
     Mtot = np.sum(Mlst)
     return(Llst,Dlst,Mlst, Ltot, Dtot, Mtot)
 
-Llst_0,Dlst_0,Mlst_0, Ltot_0, Dtot_0, Mtot_0 = aero_loads(xlst_0, ylst_0,Cllst_0, Cdlst_0, Cmlst_0)
-Llst_10,Dlst_10,Mlst_10, Ltot_10, Dtot_10, Mtot_10 = aero_loads(xlst_10, ylst_10,Cllst_10, Cdlst_10, Cmlst_10)
-    return(ylst,Cllst, Cdlst, Cmlst)
+    Llst_0,Dlst_0,Mlst_0, Ltot_0, Dtot_0, Mtot_0 = aero_loads(xlst_0, ylst_0,Cllst_0, Cdlst_0, Cmlst_0)
+    Llst_10,Dlst_10,Mlst_10, Ltot_10, Dtot_10, Mtot_10 = aero_loads(xlst_10, ylst_10,Cllst_10, Cdlst_10, Cmlst_10)
 
-ylst_0,Cllst_0, Cdlst_0, Cmlst_0 = aero_coefficient(aero_data_AOA_0)
-ylst_10,Cllst_10, Cdlst_10, Cmlst_10 = aero_coefficient(aero_data_AOA_10)
+
+
+
 
