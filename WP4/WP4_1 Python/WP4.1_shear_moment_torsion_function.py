29707220c9d019fa3a021c60744bdf195f0b5c78
BillieSeffer <b.r.n.seffer@student.tudelft.nl>
2021-11-11 17:12:30 +0100

    Update WP4.1_shear_moment_torsion_function.py

--- WP4/WP4_1 Python/WP4.1_shear_moment_torsion_function.py
+++ WP4/WP4_1 Python/WP4.1_shear_moment_torsion_function.py
@@ -3,19 +3,42 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy as sp
-from scipy import interpolate
-from scipy import integrate
+import sympy as sym
 
-def y (x) :
-    return x ** 3 
+#define x as a symbol 
+x = sym.Symbol('x')
 
-print(sp.integrate.quad(y,0,12))
+# define function
+def f1(x) :
+    return x
+
+def f2 (x) :
+    return 2 * x ** 3
+
+# shear calculator 
+def shear(f, x):
+    S = sym.integrate(f,x) * (-1)
+    return S
 
 
+# bending calculator 
+def bending(f,x) :
+    S = sym.integrate(f,x)
+    B = sym.integrate(S,x)
+    return B
 
 
+print(shear( f1(x) , x ) )
+print(bending( f1(x) , x ) )
+
+sfunction = shear( f1(x) , x )
+bfunction = bending( f1(x) , x )
+
+sym.plot(sfunction,(x,0,12.32))
+sym.plot(bfunction,(x,0,12.32))
 
 
+ 
 
 
 
