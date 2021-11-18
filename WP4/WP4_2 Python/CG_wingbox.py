2101fed15090727303c211bbe5c641311eb79b83
lynnvorgers <lynn.vorgers2001@gmail.com>
2021-11-18 16:53:31 +0100

    updated

--- WP4/WP4_2 Python/CG_wingbox.py
+++ WP4/WP4_2 Python/CG_wingbox.py
@@ -1,4 +1,4 @@
-from math import tan, pi
+from math import tan, pi 
 
 def cg_calculation(Theta2, Theta3, b2, DeltaX):
     
@@ -7,12 +7,12 @@ def cg_calculation(Theta2, Theta3, b2, DeltaX):
     c = DeltaX * tan(Theta3)
 
     x1 = DeltaX / 2
-    x2 = DeltaX / 3
+    x2 = c / 3
     x3 = DeltaX /3
 
     z3 = DeltaX * 2/3
-    z1 = z3 + (b2 /2 )
-    z2 = z1 + (c/3)
+    z1 = c + (b2 /2 )
+    z2 = c + b2 + (c/3)
 
     A1 = b2 * DeltaX
     A2 = 1/2 * a * DeltaX
@@ -23,3 +23,5 @@ def cg_calculation(Theta2, Theta3, b2, DeltaX):
     CG_z = ((z1 * A1) + (z2 * A2) + (z3 * A3))/(A1 + A2 + A3)
 
     return CG_x, CG_z
+#This is basically wrong, need to consider edges instead of the inner area
+#Also need to introduce stringers
