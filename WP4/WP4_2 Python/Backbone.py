08c544acda99a5efc9457461859cff30caec1c86
Blackbirdie72 <f.w.a.sprangers@student.tudelft.nl>
2021-11-18 15:12:54 +0100

    Duke of LaTeX, Lord Protector of Overleaf, Master over the document, Apostle of the AIAA, etc., etc.

--- WP4/WP4_2 Python/Backbone.py
+++ WP4/WP4_2 Python/Backbone.py
@@ -17,7 +17,7 @@ j = 0
 CG_XList = []
 CG_ZList = []
 Ix_totalList = []
-Iy_totalList = []
+Iz_totalList = []
 
 SpanTab = []
 Ixtab = [] 
@@ -28,10 +28,10 @@ import CG_wingbox as CG
 import Moment_of_Inertia as MoI
 
 
-Theta2, Theta3, b2, DeltaX, Cr, y = Lengths.WingboxDimensions(RCr, TCr, Span, dT)
+alpha, beta, b2, DeltaX, Cr, y = Lengths.WingboxDimensions(RCr, TCr, Span, dT)
 
 while (dT * i)<= Span/2 :
-    CG_x, CG_z = CG.cg_calculation(Theta2, Theta3, b2[i], DeltaX[i])
+    CG_x, CG_z = CG.cg_calculation(alpha, beta, b2[i], DeltaX[i])
     CG_XList.append(CG_x)
     CG_ZList.append(CG_z)
     i = i +1
@@ -40,8 +40,8 @@ t = 1 # Needs to be defined somewhere!! (not here)
 
 
 while (dT * j)<= Span/2 :
-    Ix_total = MoI.Ixcalculator(DeltaX[j],b2[j],Theta2,Theta3,t,CG_XList[j],CG_ZList[j])
-    Iz_total = MoI.Izcalculator(DeltaX[j],b2[j],Theta2,Theta3,t,CG_XList[j],CG_ZList[j])
+    Ix_total = MoI.Ixcalculator(DeltaX[j],b2[j],alpha,beta,t,CG_XList[j],CG_ZList[j])
+    Iz_total = MoI.Izcalculator(DeltaX[j],b2[j],alpha,beta,t,CG_XList[j],CG_ZList[j])
     Ix_totalList.append(Ix_total)
     Iz_totalList.append(Iz_total)
     SpanTab.append(dT * j)
@@ -65,7 +65,7 @@ plt.show()
 # [ ] Think how you want your resutls to end up, do you want a list and if so, which values do you want to know
 # [ ] Think how to inplement the data from WP4.1 load diagrams
 # [ ] Putting the stringer MoI in backbone
-# [ ] Making all the values in the other code use the same name
+# [V] Making all the values in the other code use the same name
 # [ ] Making the graphs more nice
 # [ ] Putting formulas in the code (Lynn)
 #  
