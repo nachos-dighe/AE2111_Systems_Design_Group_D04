8840e5c044ce0d94e3cb5c12f1d3608530318f7b
Blackbirdie72 <f.w.a.sprangers@student.tudelft.nl>
2021-11-23 13:39:45 +0100

    CG

--- WP4/WP4_2 Python/CG_wingbox.py
+++ WP4/WP4_2 Python/CG_wingbox.py
@@ -1,5 +1,12 @@
 from math import tan, pi
 
+Theta2 = (2.54) * 0.0174532925
+Theta3 = (0.73) * 0.0174532925
+b2     = 5.42
+DeltaX = 55
+
+
+
 def cg_calculation(Theta2, Theta3, b2, DeltaX):
     
     #intermediate outputs
@@ -7,12 +14,12 @@ def cg_calculation(Theta2, Theta3, b2, DeltaX):
     c = DeltaX * tan(Theta3)
 
     x1 = DeltaX / 2
-    x2 = DeltaX / 3
+    x2 = c / 3
     x3 = DeltaX /3
 
     z3 = DeltaX * 2/3
-    z1 = z3 + (b2 /2 )
-    z2 = z1 + (c/3)
+    z1 = c + (b2 /2 )
+    z2 = c+b2 + (c/3)
 
     A1 = b2 * DeltaX
     A2 = 1/2 * a * DeltaX
@@ -22,4 +29,6 @@ def cg_calculation(Theta2, Theta3, b2, DeltaX):
     CG_x = ((x1 * A1)+ (x2 * A2) + (x3 * A3))/(A1+ A2 + A3)
     CG_z = ((z1 * A1) + (z2 * A2) + (z3 * A3))/(A1 + A2 + A3)
 
-    return CG_x, CG_z
+    return CG_x, CG_z, c, a
+CG_x, CG_z, c, a = cg_calculation(Theta2, Theta3, b2, DeltaX)
+print(CG_x, CG_z, c, a)
