c5f9f67e1bd4eb6b82b7d977d6abc9d25dd9d47f
Berke <pakbeseb@gmail.com>
2021-11-24 20:34:01 +0100

    Update Moment_of_Inertia.py

--- WP4/WP4_2 Python/Moment_of_Inertia.py
+++ WP4/WP4_2 Python/Moment_of_Inertia.py
@@ -4,6 +4,9 @@ import scipy as sp
 from scipy import interpolate
 
 from math import tan
+from math import sin
+from math import cos
+from math import pi
 import math
 
 
@@ -13,48 +16,31 @@ import math
 
 #coordinate system: positive z up, positive x right
 
-def Ixcalculator(deltax,b,alpha,beta,t,x,z):
+def Ixxcalculator(deltax,b,alpha,beta,Ixx,x,z):
+
+    Ixx = (()/(12*(cos(beta))**3)))
     
-    Ix_rectangle_outer = (1/12)* deltax * (b**3) + deltax * b * (z - (deltax*tan(beta)+(b/2)))**2 
-    
-    Ix_upper_triangle_outer = (1/36) * deltax * ((deltax* tan(alpha))**3) + ((deltax*deltax*tan(alpha))/2)*(deltax*tan(beta)+b+(1/3)*deltax*tan(alpha)-z)**2
-
-    Ix_lower_triangle_outer = (1/36) * deltax * ((deltax* tan(beta))**3) + ((deltax*deltax*tan(beta))/2)*(z-((2/3)*deltax*tan(beta)))**2 
-
-    Ix_rectangle_inner = (1/12)*(deltax-2*t)*((b-2*t)**3)+(deltax-2*t)*(b-2*t)*(z-(t+(deltax-2*t)*tan(beta)+((b-2*t)/2)))**2 
-
-    Ix_upper_triangle_inner = (1/36) * (deltax-2*t)*(((deltax-2*t)*tan(alpha))**3)+(((deltax-2*t)*(deltax-2*t)*tan(alpha))/2)*(t+(deltax-2*t)*tan(beta)+(b-2*t)+(((deltax-2*t)*tan(alpha))/3) - z)**2 
-
-    Ix_lower_triangle_inner = (1/36) * (deltax-2*t)*(((deltax-2*t)*tan(beta))**3)+(((deltax-2*t)*(deltax-2*t)*tan(beta))/2)*(z-(t+(2/3)*(deltax-2*t)*tan(beta)))**2 
-
-    Ix_total = Ix_rectangle_outer + Ix_upper_triangle_outer + Ix_lower_triangle_outer - Ix_upper_triangle_inner - Ix_lower_triangle_inner - Ix_rectangle_inner
-
-    return (Ix_total)
+    return (Ixx_total)
 
 
 
 
    
-def Izcalculator(deltax,b,alpha,beta,t,x,z):
+def Izzcalculator(deltax,b,alpha,beta,t,x,z):
 
-    Iz_rectangle_outer = (1/12) * (deltax**3) * b + deltax * b * ((deltax/2)-x)**2 
+    Izz1 =
 
-    Iz_upper_triangle_outer = (1/36) * (deltax**3) * deltax * tan(alpha) + ((deltax*deltax*tan(alpha))/2)*(x- (deltax/3))**2 
+    Izz2 =
 
-    Iz_lower_triangle_outer = (1/36) * (deltax**3) * deltax * tan(beta) + ((deltax*deltax*tan(beta))/2)*(x- (deltax/3))**2 
+    Izz3 =
 
-    Iz_rectangle_inner = (1/12) * ((deltax - 2 *t)**3)*(b-2*t) + (deltax-2*t) * (b - 2*t)*((t+(1/2)*(deltax-2*t))-x)**2 
-
-    Iz_upper_triangle_inner = (1/36) * ((deltax-2*t)**3)*(deltax-2*t)*tan(alpha)+((((deltax-2*t)**2)*tan(alpha))/2)*(x-(t+((deltax-2*t)/3)))**2 
-
-    Iz_lower_triangle_inner = (1/36) * ((deltax-2*t)**3)*(deltax-2*t)*tan(beta)+((((deltax-2*t)**2)*tan(beta))/2)*(x-(t+((deltax-2*t)/3)))**2 
-
-    Iz_total = Iz_rectangle_outer + Iz_upper_triangle_outer + Iz_lower_triangle_outer - Iz_upper_triangle_inner - Iz_lower_triangle_inner - Iz_rectangle_inner
+    Izz4 =
 
 
-    return(Iz_total)
+    
+    return(Izz_total)
 
 
 
 
-#outputs: moment of inertia of shape for x and z direction
+#output: thickness required
