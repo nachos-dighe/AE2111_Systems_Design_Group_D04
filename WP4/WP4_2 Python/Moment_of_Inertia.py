a0c66bb8feb77c71904009adab4f392a3bca1b4f
Berke <pakbeseb@gmail.com>
2021-11-24 20:36:05 +0100

    Update Moment_of_Inertia.py

--- WP4/WP4_2 Python/Moment_of_Inertia.py
+++ WP4/WP4_2 Python/Moment_of_Inertia.py
@@ -13,34 +13,45 @@ import math
 #Angles in radians!!!
 
 
+#coordinate system: positive z down!!!!!!, positive x right
 
-#coordinate system: positive z up, positive x right
 
-def Ixxcalculator(deltax,b,alpha,beta,Ixx,x,z):
 
-    Ixx = (()/(12*(cos(beta))**3)))
+
+#inputs (line 1): deltax (horizontal distance of wingbox), beta lower profile angle to horizontal axis, alpha upper profile angle to horizontal axis, z position of cg vertically wrt datum
+
+#inputs (line 2): x horizontal position of cg from the datum, b height shorter vertical profile, Ixx & Iyy obtained from stress calculations
+
+
+
+
+Ixx1  =((((deltax)**3)*((sin(beta))**2))/(12*((cos(beta))**3)))+((deltax)/cos(beta))*(-z+((deltax * tan(beta))/2))**2 #lower left angled profile
     
-    return (Ixx_total)
+Ixx2 = ((((deltax)**3)*((sin((pi/2)-alpha))**2))/(12*((cos(alpha))**3)))+(deltax/(cos(alpha)))*((-deltax*tan(beta))-b-((deltax*tan(alpha))/2)+z)**2 #upper right angled profile
+
+Ixx3 = 1/12 * ((deltax*tan(beta)+b+deltax*tan(alpha))**3) +(deltax*tan(beta)+b+deltax*tan(alpha))*(z-((deltax*tan(beta)+b+deltax*tan(alpha))/2))**2 #vertical profile on the left
+
+Ixx4 = 1/12 * b**3 + b*(deltax*tan(beta)+(b/2)-z)**2 #vertical profile on the right
 
 
 
+Iyy1 = ((((deltax)**3)*((cos(beta))**2))/(12*((cos(beta))**3)))+((deltax)/cos(beta))*(x-(deltax/2))**2    #lower left angled profile
 
-   
-def Izzcalculator(deltax,b,alpha,beta,t,x,z):
+Iyy2 = ((((deltax)**3)*((cos((pi/2)-alpha))**2))/(12*((cos(alpha))**3)))+(deltax/(cos(alpha)))*(x-(deltax/2))**2   #upper right angled profile
 
-    Izz1 =
+Iyy3 = (deltax * tan(beta) + b + deltax * tan(alpha))*x**2  #vertical profile on the left
 
-    Izz2 =
-
-    Izz3 =
-
-    Izz4 =
-
-
+Iyy4 = b*(deltax-x)**2   #vertical profile on the right
     
-    return(Izz_total)
+
+#tx is thickness determined by using Ixx. We will obtain ty from Iyy. Whichever is greater is the design thickness as we have to satisfy both cases
+
+tx = Ixx/(Ixx1+Ixx2+Ixx3+Ixx4)
+ty = Iyy/(Iyy1+Iyy2+Iyy3+Iyy4)
+
+t_minimum = max(tx, ty)
+
+#output: t_minimum is the thinnest the structure should be to barely withstand the loads (chosen as the maximum of tx and ty)
 
 
 
-
-#output: thickness required
