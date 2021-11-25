8d790c246a8c5e34e52283203b95b43201b8ff50
Berke <pakbeseb@gmail.com>
2021-11-25 14:10:00 +0100

    Update Moment_of_Inertia.py

--- WP4/WP4_2 Python/Moment_of_Inertia.py
+++ WP4/WP4_2 Python/Moment_of_Inertia.py
@@ -20,36 +20,51 @@ import math
 
 #inputs (line 1): deltax (horizontal distance of wingbox), beta lower profile angle to horizontal axis, alpha upper profile angle to horizontal axis, z position of cg vertically wrt datum
 
-#inputs (line 2): x horizontal position of cg from the datum, b height shorter vertical profile, Ixx & Iyy obtained from stress calculations
+#inputs (line 2): x horizontal position of cg from the datum, b height shorter vertical profile, Ixx_required & Iyy_required are obtained from stress calculations
 
 
 
-
-Ixx1  =((((deltax)**3)*((sin(beta))**2))/(12*((cos(beta))**3)))+((deltax)/cos(beta))*(-z+((deltax * tan(beta))/2))**2 #lower left angled profile
+def Ixx(deltax,beta,alpha,x,z,b):
     
-Ixx2 = ((((deltax)**3)*((sin((pi/2)-alpha))**2))/(12*((cos(alpha))**3)))+(deltax/(cos(alpha)))*((-deltax*tan(beta))-b-((deltax*tan(alpha))/2)+z)**2 #upper right angled profile
+    Ixx1  =((((deltax)**3)*((sin(beta))**2))/(12*((cos(beta))**3)))+((deltax)/cos(beta))*(-z+((deltax * tan(beta))/2))**2 #lower left angled profile
+    
+    Ixx2 = ((((deltax)**3)*((sin((pi/2)-alpha))**2))/(12*((cos(alpha))**3)))+(deltax/(cos(alpha)))*((-deltax*tan(beta))-b-((deltax*tan(alpha))/2)+z)**2 #upper right angled profile
 
-Ixx3 = 1/12 * ((deltax*tan(beta)+b+deltax*tan(alpha))**3) +(deltax*tan(beta)+b+deltax*tan(alpha))*(z-((deltax*tan(beta)+b+deltax*tan(alpha))/2))**2 #vertical profile on the left
+    Ixx3 = 1/12 * ((deltax*tan(beta)+b+deltax*tan(alpha))**3) +(deltax*tan(beta)+b+deltax*tan(alpha))*(z-((deltax*tan(beta)+b+deltax*tan(alpha))/2))**2 #vertical profile on the left
 
-Ixx4 = 1/12 * b**3 + b*(deltax*tan(beta)+(b/2)-z)**2 #vertical profile on the right
+    Ixx4 = 1/12 * b**3 + b*(deltax*tan(beta)+(b/2)-z)**2 #vertical profile on the right
+
+    Ixx = Ixx1+Ixx2+Ixx3+Ixx4
+
+    return Ixx
+
+def Iyy(deltax,beta,alpha,x,z,b):
 
 
+    Iyy1 = ((((deltax)**3)*((cos(beta))**2))/(12*((cos(beta))**3)))+((deltax)/cos(beta))*(x-(deltax/2))**2    #lower left angled profile
 
-Iyy1 = ((((deltax)**3)*((cos(beta))**2))/(12*((cos(beta))**3)))+((deltax)/cos(beta))*(x-(deltax/2))**2    #lower left angled profile
+    Iyy2 = ((((deltax)**3)*((cos((pi/2)-alpha))**2))/(12*((cos(alpha))**3)))+(deltax/(cos(alpha)))*(x-(deltax/2))**2   #upper right angled profile
+    
+    Iyy3 = (deltax * tan(beta) + b + deltax * tan(alpha))*x**2  #vertical profile on the left
 
-Iyy2 = ((((deltax)**3)*((cos((pi/2)-alpha))**2))/(12*((cos(alpha))**3)))+(deltax/(cos(alpha)))*(x-(deltax/2))**2   #upper right angled profile
+    Iyy4 = b*(deltax-x)**2   #vertical profile on the right
 
-Iyy3 = (deltax * tan(beta) + b + deltax * tan(alpha))*x**2  #vertical profile on the left
+    Iyy = Iyy1+Iyy2+Iyy3+Iyy4
 
-Iyy4 = b*(deltax-x)**2   #vertical profile on the right
+    return Iyy
+
+
+def thickness_selection(Ixx,Iyy,Ixx_required,Iyy_required):
     
 
 #tx is thickness determined by using Ixx. We will obtain ty from Iyy. Whichever is greater is the design thickness as we have to satisfy both cases
 
-tx = Ixx/(Ixx1+Ixx2+Ixx3+Ixx4)
-ty = Iyy/(Iyy1+Iyy2+Iyy3+Iyy4)
+    tx = Ixx_required/Ixx
+    ty = Iyy_required/Iyy
 
-t_minimum = max(tx, ty)
+    t_minimum = max(tx, ty)
+
+    return(t_minimum)
 
 #output: t_minimum is the thinnest the structure should be to barely withstand the loads (chosen as the maximum of tx and ty)
 
