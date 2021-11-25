c943312fedb60b6eb468fd39361ec07dcb4204c8
Blackbirdie72 <f.w.a.sprangers@student.tudelft.nl>
2021-11-25 14:15:00 +0100

    Graham Chapman

--- WP4/WP4_2 Python/Moment_of_Inertia.py
+++ WP4/WP4_2 Python/Moment_of_Inertia.py
@@ -18,38 +18,38 @@ import math
 
 
 
-#inputs (line 1): deltax (horizontal distance of wingbox), beta lower profile angle to horizontal axis, alpha upper profile angle to horizontal axis, z position of cg vertically wrt datum
+#inputs (line 1): DeltaX (horizontal distance of wingbox), beta lower profile angle to horizontal axis, alpha upper profile angle to horizontal axis, z position of cg vertically wrt datum
 
 #inputs (line 2): x horizontal position of cg from the datum, b height shorter vertical profile, Ixx_required & Izz_required are obtained from stress calculations
 
 
 
-def Ixx(deltax,beta,alpha,x,z,b):
+def Ixx(DeltaX,beta,alpha,x,z,b):
     
-    Ixx1  =((((deltax)**3)*((sin(beta))**2))/(12*((cos(beta))**3)))+((deltax)/cos(beta))*(-z+((deltax * tan(beta))/2))**2 #lower left angled profile
+    Ixx1  =((((DeltaX)**3)*((sin(beta))**2))/(12*((cos(beta))**3)))+((DeltaX)/cos(beta))*(-z+((DeltaX * tan(beta))/2))**2 #lower left angled profile
     
-    Ixx2 = ((((deltax)**3)*((sin((pi/2)-alpha))**2))/(12*((cos(alpha))**3)))+(deltax/(cos(alpha)))*((-deltax*tan(beta))-b-((deltax*tan(alpha))/2)+z)**2 #upper right angled profile
+    Ixx2 = ((((DeltaX)**3)*((sin((pi/2)-alpha))**2))/(12*((cos(alpha))**3)))+(DeltaX/(cos(alpha)))*((-DeltaX*tan(beta))-b-((DeltaX*tan(alpha))/2)+z)**2 #upper right angled profile
 
-    Ixx3 = 1/12 * ((deltax*tan(beta)+b+deltax*tan(alpha))**3) +(deltax*tan(beta)+b+deltax*tan(alpha))*(z-((deltax*tan(beta)+b+deltax*tan(alpha))/2))**2 #vertical profile on the left
+    Ixx3 = 1/12 * ((DeltaX*tan(beta)+b+DeltaX*tan(alpha))**3) +(DeltaX*tan(beta)+b+DeltaX*tan(alpha))*(z-((DeltaX*tan(beta)+b+DeltaX*tan(alpha))/2))**2 #vertical profile on the left
 
-    Ixx4 = 1/12 * b**3 + b*(deltax*tan(beta)+(b/2)-z)**2 #vertical profile on the right
+    Ixx4 = 1/12 * b**3 + b*(DeltaX*tan(beta)+(b/2)-z)**2 #vertical profile on the right
 
     Ixx = Ixx1+Ixx2+Ixx3+Ixx4
 
     return Ixx
 
-def Izz(deltax,beta,alpha,x,z,b):
+def Izz(DeltaX,beta,alpha,x,z,b):
 
 
-    Izz1 = ((((deltax)**3)*((cos(beta))**2))/(12*((cos(beta))**3)))+((deltax)/cos(beta))*(x-(deltax/2))**2    #lower left angled profile
+    Izz1 = ((((DeltaX)**3)*((cos(beta))**2))/(12*((cos(beta))**3)))+((DeltaX)/cos(beta))*(x-(DeltaX/2))**2    #lower left angled profile
 
-    Izz2 = ((((deltax)**3)*((cos((pi/2)-alpha))**2))/(12*((cos(alpha))**3)))+(deltax/(cos(alpha)))*(x-(deltax/2))**2   #upper right angled profile
+    Izz2 = ((((DeltaX)**3)*((cos((pi/2)-alpha))**2))/(12*((cos(alpha))**3)))+(DeltaX/(cos(alpha)))*(x-(DeltaX/2))**2   #upper right angled profile
     
-    Izz3 = (deltax * tan(beta) + b + deltax * tan(alpha))*x**2  #vertical profile on the left
+    Izz3 = (DeltaX * tan(beta) + b + DeltaX * tan(alpha))*x**2  #vertical profile on the left
 
-    Izz4 = b*(deltax-x)**2   #vertical profile on the right
+    Izz4 = b*(DeltaX-x)**2   #vertical profile on the right
 
-    Izz = Izz1+Izz2+Izz3+Izz4
+    Izz = Izz1 + Izz2 + Izz3 + Izz4
 
     return Izz
 
