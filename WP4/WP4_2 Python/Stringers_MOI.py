259742672ff5fb2fe36faca65650dc820f869834
lynnvorgers <lynn.vorgers2001@gmail.com>
2021-11-25 14:36:03 +0100

    Update Stringers_MOI.py
    Corrected cg of stringers for MOI calculation

--- WP4/WP4_2 Python/Stringers_MOI.py
+++ WP4/WP4_2 Python/Stringers_MOI.py
@@ -17,30 +17,32 @@ def moi_stringers(nr_top, nr_bot, L_s, t, t_s, alpha, beta, b, deltax):
     
 #Intermediate outputs for the moment of inertia
     A = (2*L_s*t_s)-(t_s**2)      #Area of one stringer
+    cg = (1.5*L_s*L_s - L_s*t_s)/(2*L_s - t_s)
 
-#Positions in terms of x and z of the stringers (1-4 being the corner stringers) measured from the most left outer corner of the wingbox
-    x1 = t+(0.5*t_s)
-    z1 = t+(0.5*t_s)
-    x2 = t+(0.5*t_s)
-    z2 = b + deltax*(tan(beta)+tan(alpha)) - (t+(0.5*t_s))
-    x3 = deltax - (t+(0.5*t_s))
-    z3 = b + deltax*tan(beta) - (t+(0.5*t_s))
-    x4 = deltax - (t+(0.5*t_s))
-    z4 = deltax*tan(beta) + (t+(0.5*t_s))
+#Positions cg of stringersin terms of x and z of the stringers
+#(1-4 being the corner stringers) measured from the bottem left outer corner of the wingbox
+    x1 = t+(cg)
+    z1 = t+(cg)
+    x2 = t+(cg)
+    z2 = b + deltax*(tan(beta)+tan(alpha)) - (t+(cg))
+    x3 = deltax - (t+(cg))
+    z3 = b + deltax*tan(beta) - (t+(cg))
+    x4 = deltax - (t+(cg))
+    z4 = deltax*tan(beta) + (t+(cg))
 
 #spacing of the stringers and introducing this into the dimensions
-    L_top = (deltax/cos(alpha)) - (2*t) -t_s         #Length of top/bottem of wingbox where a stringer can be placed (from cg of left corner stringer to cg of left corner stringer)
-    L_bot = (deltax/cos(beta)) - (2*t) -t_s       
+    L_top = (deltax/cos(alpha)) - (2*t) - cg         #Length of top/bottem of wingbox where a stringer can be placed (from cg of left corner stringer to cg of left corner stringer)
+    L_bot = (deltax/cos(beta)) - (2*t) - cg       
     s_top = L_top/(nr_top-1)                         #Length from cg to cg of the stringers
     s_bot = L_bot/(nr_bot-1)
 
     lst_stringers = []
-    for i in range(0,Nr_top):
+    for i in range(0,nr_top):
         x = x1 + i * (s_top * cos(alpha))
         z = z1 - i * (s_top * sin(alpha))
         d = ((x*x)+(z*z))**0.5
         lst_stringers.append(d)
-    for j in range(0,Nr_bot):
+    for j in range(0,nr_bot):
         x = x1 + j * (s_bot * cos(beta))
         z = z1 + j * (s_bot * sin(beta))
         d = ((x*x)+(z*z))**0.5
@@ -53,7 +55,7 @@ def moi_stringers(nr_top, nr_bot, L_s, t, t_s, alpha, beta, b, deltax):
 
     return I_s, A
 #Need to have a list of the positions of the stringers, x and z
-
+print(moi_stringers(nr_top, nr_bot, L_s, t, t_s, alpha, beta, b, deltax))
 
 
 
