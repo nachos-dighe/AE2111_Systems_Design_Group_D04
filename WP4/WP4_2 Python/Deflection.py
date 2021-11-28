595a8256815c85f497df18ebf287c4b100f10cdf
Blackbirdie72 <f.w.a.sprangers@student.tudelft.nl>
2021-11-28 15:28:54 +0100

    .

--- WP4/WP4_2 Python/Deflection.py
+++ WP4/WP4_2 Python/Deflection.py
@@ -17,7 +17,7 @@ def deflection(M_lst, I_lst, E, ylst):
 
     return Defl_lst
 
-Defl_lst = deflection(M_lst, I_lst, E, ylst0)
+##Defl_lst = deflection(M_lst, I_lst, E, ylst0)
 ##print(Defl_lst)
 ##
 ##plt.plot(Defl_lst, ylst0, 'rs-', label='Deflection')
