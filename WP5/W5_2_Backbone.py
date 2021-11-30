09d6d1e30bdb3ae8e558c32c85ac2eda78dd7fab
Blackbirdie72 <f.w.a.sprangers@student.tudelft.nl>
2021-11-30 14:59:32 +0100

    .1

---
+++ WP5/W5_2_Backbone.py
@@ -0,0 +1,20 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy as sp
+from scipy import interpolate
+
+
+from math import *
+
+#---------------------------------------------------------------------------------------------
+
+# Geometric
+RCr = 4.4 # [m] Root chord
+TCr = 1.76 # [m] Tip chord
+Span = 24.64 # [m] Span
+dT = 0.1
+
+# Counters
+i = 0 
+j = 0 
+k = 0
