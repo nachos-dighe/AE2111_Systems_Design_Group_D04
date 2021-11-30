6db8857137c2326e8a6d542b8a32cdc8aa32209c
lynnvorgers <lynn.vorgers2001@gmail.com>
2021-11-30 15:55:05 +0100

    .

--- WP4/WP4_2 Python/Deflection.py
+++ WP4/WP4_2 Python/Deflection.py
@@ -4,10 +4,6 @@ import ylst
 import scipy.integrate as sp
 import numpy as np
 from matplotlib import pyplot as plt
-ylst0 = [1,2,3,4,5]  # The positions calcualted along the span
-M_lst = [1,2,3,4,5]  # The moment loads along the span 
-I_lst = [2,4,6,8,10] # The moment fo inertia along the span
-E = 70.42 * 10**9    # The E-modulus [Pa]
 
 def deflection(M_lst, I_lst, ylst):
     num = I_lst 
@@ -16,14 +12,4 @@ def deflection(M_lst, I_lst, ylst):
     Defl_lst = sp.cumtrapz(Defl_lst1,ylst,initial=0)
 
     return Defl_lst
-
-##Defl_lst = deflection(M_lst, I_lst, E, ylst0)
-##print(Defl_lst)
-##
-##plt.plot(Defl_lst, ylst0, 'rs-', label='Deflection')
-##plt.axhline(0, color="black")
-##plt.axvline(0, color="black")
-##plt.legend()
-##plt.grid()
-##plt.show()
 
