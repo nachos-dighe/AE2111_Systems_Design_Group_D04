330898bcb8a8b3c06680d3afbe9e2f1325df7f58
BillieSeffer <b.r.n.seffer@student.tudelft.nl>
2021-12-12 14:00:40 +0100

    sss

--- WP5/WP5_1_shearflow_extra_simple.py
+++ WP5/WP5_1_shearflow_extra_simple.py
@@ -1,10 +1,29 @@
+import numpy as np 
 from WP4_XFLR5_Raw_Data import *
 from WP4_XFLR5_Raw_Data import xlst_0
 from WP4_1_main import *
 from WP4_1_main import Vres_poscrit, Vres_negcrit, TMres_poscrit ,TMres_negcrit
 
+xlst = xlst_0
 
-##xlst = xlst_0
+#tau_crit =( pi^2 * k_s * E ) / 12(1-nu^2) * (t/b)^2
+E = 68.9 * 10 ** 9
+vu = 0.33
+t = 0.00198
+h_f = 0.0856 * xlst
+h_r = 0.0542 * xlst 
+
+def tau_critical(k_s, h, n_ribs):
+    spacing = 12.35 / n_ribs    #first assuming equal rib spacing, iterate later
+    if spacing <= h:
+        b = spacing
+    if spacing >= h:
+        b = h 
+    tau_cr =  (( np.pi ** 2 * k_s * E ) / ( 12 * ( 1 - nu ) ** 2 ) ) \
+         * ( ( t / b ) ** 2 )
+    return tau_cr 
+
+
 ##
 ### shear flow due to shear
 ##
