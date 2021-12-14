e1aaa5affec6e21c12a4f4a7da65a6772e34bfce
BillieSeffer <b.r.n.seffer@student.tudelft.nl>
2021-12-14 11:58:01 +0100

    Update WP5_1_shearflow_extra_simple.py

--- WP5/WP5_1_shearflow_extra_simple.py
+++ WP5/WP5_1_shearflow_extra_simple.py
@@ -25,6 +25,13 @@ ab_lst = np.linspace(min(ab_lst_init), max(ab_lst_init), num=1000, endpoint=True
 ks_interp = sp.interpolate.interp1d(ab_lst_init, ks_lst_init, kind = "cubic", fill_value="extrapolate")
 ks_lst = ks_interp(ab_lst)
 
+clamped_init = [15, 13, 12, 11, 10.5, 10, 9.8, 9.6, 9.8, 9.6, 9.5, 9.5, 9.5, 9.5, 9.5, 9.5, 9.5]
+clamped_interp = sp.interpolate.interp1d(ab_lst_init, clamped_init, kind = "cubic", fill_value="extrapolate")
+clamped_lst = clamped_interp(ab_lst)
+
+plt.plot(ab_lst, clamped_lst)
+plt.show()
+
 
 
 def tau_critical(h, n_ribs):
@@ -49,18 +56,33 @@ def tau_critical(h, n_ribs):
     frac = a / b
 
     print(frac)
-    k_s = np.interp(frac, ab_lst, ks_lst) 
+    k_s =  np.interp(frac, ab_lst, ks_lst)
+    k_s[0] = np.interp(frac, ab_lst, clamped_lst)[0]
 
     tau_cr =(( np.pi ** 2 * k_s * E ) / ( 12 * ( 1 - nu ) ** 2 ) ) * ( ( t / b ) ** 2 )
 
     return tau_cr
 
-t_crit_f_4 = tau_critical( h_f, 4)
+t_crit_f_4 = tau_critical( h_f, 4, )
 t_crit_f_12 = tau_critical( h_f, 12)
 t_crit_f_24 = tau_critical( h_f, 24)
 t_crit_f_36 = tau_critical( h_f, 36)
+t_crit_f_60 = tau_critical( h_f, 60)
 t_crit_f_100 = tau_critical( h_f, 100)
-t_crit_r = tau_critical( h_r, 12)
+
+t_crit_r_4 = tau_critical( h_r, 4, )
+t_crit_r_12 = tau_critical( h_r, 12)
+t_crit_r_24 = tau_critical( h_r, 24)
+t_crit_r_36 = tau_critical( h_r, 36)
+t_crit_r_60 = tau_critical( h_r, 60)
+t_crit_r_100 = tau_critical( h_r, 100)
+
+print("crit load pos rear spar 100 ribs:", t_crit_r_100)
+
+
+
+
+
 
 
 # shear flow due to shear   # tau_av = V / ( h_f * t_f + h_r * t_r )
@@ -76,39 +98,53 @@ def shearstress_T (t, Tlst):
     tau_T = q / t
     return tau_T
 
-#call functions 
+#call functions
+print(0.0865 * xlst)
+print(t_r)
 
-tau_tot_f_pos = shearstress_ave(Vres_poscrit, t_f, t_r) * 1.5 + shearstress_T(t_f, TMres_poscrit)
-tau_tot_r_pos = shearstress_ave(Vres_poscrit, t_f, t_r) * 1.5 - shearstress_T(t_f, TMres_poscrit)
 
-tau_tot_f_neg = shearstress_ave(Vres_negcrit, t_f, t_r) * 1.5 + shearstress_T(t_f, TMres_negcrit)
-tau_tot_r_neg = shearstress_ave(Vres_negcrit, t_f, t_r) * 1.5 - shearstress_T(t_f, TMres_negcrit)
+print('internal shear force is:', Vres_poscrit)
+print()
+print("average shearstress front positive is:",shearstress_ave(Vres_poscrit, t_f, t_r) * 1.5)
+print()
+print("average shearstress rear positive is:", shearstress_ave(Vres_poscrit, t_f, t_r) * 1.5)
+print()
+print("shearstress due to torque:", shearstress_T(t_f, TMres_poscrit) )
+
+tau_tot_f_pos = np.absolute(shearstress_ave(Vres_poscrit, t_f, t_r)) * 1.5 #+ shearstress_T(t_f, TMres_poscrit))
+tau_tot_r_pos = np.absolute(shearstress_ave(Vres_poscrit, t_f, t_r) * 1.5 - shearstress_T(t_f, TMres_poscrit))
+
+tau_tot_f_neg = np.absolute(shearstress_ave(Vres_negcrit, t_f, t_r)) * 1.5 #+ shearstress_T(t_f, TMres_negcrit))
+tau_tot_r_neg = np.absolute(shearstress_ave(Vres_negcrit, t_f, t_r) * 1.5 - shearstress_T(t_f, TMres_negcrit))
 
 #plotting
-plt.subplot(4,1,1)
+plt.subplot(4,1,1)  # positive front 
 plt.plot(ylst, tau_tot_f_pos)
-plt.plot(ylst, t_crit_f_24)
+plt.plot(ylst,t_crit_f_36)
+plt.plot(ylst,t_crit_f_60)
+plt.plot(ylst,t_crit_f_100)
 plt.title("positive loadcase front spar")
+plt.legend(['tau max', '36 ribs', '60 ribs', '100 ribs'], loc='upper left')
 
 plt.subplot(4,1,2)
 plt.plot(ylst, tau_tot_r_pos)
-plt.plot(ylst, t_crit_r)
+plt.plot(ylst, t_crit_r_36)
+plt.plot(ylst,t_crit_r_60 )
+plt.plot(ylst,t_crit_r_100)
 plt.title("positive loadcase rear spar")
 
 plt.subplot(4,1,3)
 plt.plot(ylst, tau_tot_f_neg)
-plt.plot(ylst, t_crit_f_4)
-plt.plot(ylst,t_crit_f_12)
-plt.plot(ylst,t_crit_f_24)
 plt.plot(ylst,t_crit_f_36)
+plt.plot(ylst,t_crit_f_60)
 plt.plot(ylst,t_crit_f_100)
-
-plt.legend(['tau max','4 ribs', '12 ribs', '24 ribs', '36 ribs'], loc='upper left')
 plt.title("negative loadcase front spar")
 
 plt.subplot(4,1,4)
 plt.plot(ylst, tau_tot_r_neg)
-plt.plot(ylst, t_crit_r)
+plt.plot(ylst, t_crit_r_36)
+plt.plot(ylst,t_crit_r_60 )
+plt.plot(ylst,t_crit_r_100)
 plt.title("negative loadcase rear spar")
 
 plt.show()
