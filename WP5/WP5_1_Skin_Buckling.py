378b46a5abb7360769355053d4bbbb28096b97e6
Blackbirdie72 <f.w.a.sprangers@student.tudelft.nl>
2021-12-15 21:27:13 +0100

    .

--- WP5/WP5_1_Skin_Buckling.py
+++ WP5/WP5_1_Skin_Buckling.py
@@ -5,18 +5,48 @@ import scipy as sp
 from WP4_XFLR5_Raw_Data import xlst_0, ylst_0
 #from WP4_1_main import *
 from WP4_1_main import Vres_poscrit, Vres_negcrit, TMres_poscrit ,TMres_negcrit
+import WP5_2_Chord_LengthFRANK as Lengths
+
+# Geometric
+RCr = 4.4 # [m] Root chord
+TCr = 1.76 # [m] Tip chord
+Span = 24.64 # [m] Span
+
+alpha, beta, b2, DeltaX, Cr = Lengths.WingboxDimensions(RCr, TCr, Span, ylst_0)
+
+Stress_Top_List = []
+Stress_Bottom_List = []
 
 # parameters
 xlst = xlst_0
 ylst = ylst_0
 
-##E = 68.9 * 10 ** 9
-##nu = 0.33
-##t = 0.004
-##h_f = 0.0856 * xlst
-##h_r = 0.0542 * xlst
-##t_f = 0.004
-##t_r = 0.004
+E = 68.9 * 10 ** 9
+nu = 0.33
+t = 0.004
+h_f = 0.0856 * xlst # !! Look if this is correct
+h_r = 0.0542 * xlst # !! Look if this is correct
+t_f = 0.004 # !! assumed thickness
+t_r = 0.004 # !! assumed thickness
+
+with open("Normal_Stress_Top_Panel.dat", "r") as file : # Reads the y position file 
+    Stress_Top_RAW = file.readlines()
+
+for line in Stress_Top_RAW :
+    Stress_Top = line.replace("\n", "")
+    Stress_Top = float(Stress_Top)
+    Stress_Top_List.append(Stress_Top)
+
+
+with open("Normal_Stress_Bottom_Panel.dat", "r") as file : # Reads the y position file 
+    Stress_Bottom_RAW = file.readlines()
+
+for line in Stress_Bottom_RAW :
+    Stress_Bottom = line.replace("\n", "")
+    Stress_Bottom = float(Stress_Bottom)
+    Stress_Bottom_List.append(Stress_Bottom)
+   
+
 
 #k_c generation
 ab_lst_init_B = np.arange(0.75,5.25,0.25)
@@ -47,10 +77,6 @@ kc_lst_E = kc_interp_E(ab_lst_E)
 
 
 
-##clamped_init = [15, 13, 12, 11, 10.5, 10, 9.8, 9.6, 9.8, 9.6, 9.5, 9.5, 9.5, 9.5, 9.5, 9.5, 9.5]
-##clamped_interp = sp.interpolate.interp1d(ab_lst_init, clamped_init, kind = "cubic", fill_value="extrapolate")
-##clamped_lst = clamped_interp(ab_lst)
-
 
 def Sigma_critical(h, n_stringers):
     spacing = 12.35 / (n_stringers + 1)    #first assuming equal stringer spacing, iterate later
@@ -58,50 +84,50 @@ def Sigma_critical(h, n_stringers):
 
     a = []
     b = []
+    n_ribs = 60 #!! Don't know
+    Span = 12.35 #!! check
+
 
     for i in range(len(h)):
+        b.append(Cr[i]/n_stringers)
+        a.append((Span/2)/n_ribs)
         
-        if spacing[i] <= h[i]:
-            a.append( h[i]) 
-            b.append( spacing[i] )
-
-        if spacing[i] >= h[i]:
-            b.append( h[i] )
-            a.append( spacing[i] )
 
     a = np.array(a)
     b = np.array(b)
     frac = a / b
 
     print(frac)
-    k_s =  np.interp(frac, ab_lst, ks_lst)
-    k_s[0] = np.interp(frac, ab_lst, clamped_lst)[0]
+    k_c =  np.interp(frac, ab_lst_C, kc_lst_C)
+    k_c[0] = np.interp(frac, ab_lst_B, kc_lst_B)[0]
+    k_c[999] = np.interp(frac, ab_lst_E, kc_lst_E)[999]
 
-    Sigma_cr =(( np.pi ** 2 * k_s * E ) / ( 12 * ( 1 - nu **2 ) ) ) * ( ( t / b ) ** 2 )
+    Sigma_cr =(( np.pi ** 2 * k_c * E ) / ( 12 * ( 1 - nu **2 ) ) ) * ( ( t / b ) ** 2 )
 
     return Sigma_cr
 
-t_crit_f_4 = Sigma_critical( h_f, 4, )
-t_crit_f_12 = Sigma_critical( h_f, 12)
-t_crit_f_24 = Sigma_critical( h_f, 24)
-t_crit_f_36 = Sigma_critical( h_f, 36)
-t_crit_f_60 = Sigma_critical( h_f, 60)
-t_crit_f_100 = Sigma_critical( h_f, 100)
+Sig_crit_f_1 = Sigma_critical( h_f, 1)
+Sig_crit_f_2 = Sigma_critical( h_f, 2)
+Sig_crit_f_3 = Sigma_critical( h_f, 3)
+Sig_crit_f_4 = Sigma_critical( h_f, 4)
 
-t_crit_r_4 = Sigma_critical( h_r, 4, )
-t_crit_r_12 = Sigma_critical( h_r, 12)
-t_crit_r_24 = Sigma_critical( h_r, 24)
-t_crit_r_36 = Sigma_critical( h_r, 36)
-t_crit_r_60 = Sigma_critical( h_r, 60)
-t_crit_r_100 = Sigma_critical( h_r, 100)
+ 
+plt.plot(ylst, Sig_crit_f_1)
+plt.plot(ylst, Sig_crit_f_2)
+plt.plot(ylst, Sig_crit_f_3)
+plt.plot(ylst, Sig_crit_f_4)
+plt.title("positive loadcase front spar?? NEEDS DIFFERENT TITLE")
+plt.legend(['Sigma max', '1 stringer', '2 stringers', '3 stringers', '4 stringers'], loc='upper right')
 
-
-
+plt.show()
 
 
 
-
-
+# needs to be done:
+# [ ] Put Max load in to the code
+# [ ] Make graph titles
+# [ ] Tidy up the code
+# [ ] Check code
 
 
 
