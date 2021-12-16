30f84fb2e98f86fd1f9802dfacd232d36bc8472f
Berke <pakbeseb@gmail.com>
2021-12-16 14:46:50 +0100

    moi

--- WP5/WP5_2_MOI.py
+++ WP5/WP5_2_MOI.py
@@ -43,66 +43,67 @@ import math
 
 #SOME DIMENSIONS
 
-t = 4 * 10**(-3) #meters
+t_sides = 4 * 10**(-3) #meters
 
+t = 1.98 * 10**(-3) #meters (for top and bottom sheets)
 
 ############################################################################################################################################################################################
 
 #section for wingbox MOI calculation
 
 
-def Ixx_wingbox(DeltaX,beta,alpha,CG_Z,t, b):
+def Ixx_wingbox(DeltaX,beta,alpha,CG_Z,t_side,t, b):
     
     Ixx1  =((t*((DeltaX)**3)*((sin(beta))**2))/(12*((cos(beta))**3)))+((t*DeltaX)/cos(beta))*(CG_Z+((DeltaX * tan(beta))/2))**2 #lower left angled profile
     
     Ixx2 = (t*((DeltaX)**3)*((sin((alpha))**2))/(12*((cos(alpha))**3)))+(t*DeltaX/(cos(alpha)))*((DeltaX*tan(beta))+b+((DeltaX*tan(alpha))/2)+CG_Z)**2 #upper right angled profile
 
-    Ixx3 = (1/12) * t*((DeltaX*tan(beta)+b+DeltaX*tan(alpha))**3) +t*(DeltaX*tan(beta)+b+DeltaX*tan(alpha))*(CG_Z+((DeltaX*tan(beta)+b+DeltaX*tan(alpha))/2))**2 #vertical profile on the left
+    Ixx3 = (1/12) * t_side*((DeltaX*tan(beta)+b+DeltaX*tan(alpha))**3) +t_side*(DeltaX*tan(beta)+b+DeltaX*tan(alpha))*(CG_Z+((DeltaX*tan(beta)+b+DeltaX*tan(alpha))/2))**2 #vertical profile on the left
 
-    Ixx4 = (1/12) * t * b**3 + b*t*(DeltaX*tan(beta)+(b/2)+CG_Z)**2 #vertical profile on the right
+    Ixx4 = (1/12) * t_side * b**3 + b*t_side*(DeltaX*tan(beta)+(b/2)+CG_Z)**2 #vertical profile on the right
 
     Ixx_wingbox = Ixx1 + Ixx2 + Ixx3 + Ixx4
 
     return  Ixx_wingbox
 
-Ixx_wingbox = Ixx_wingbox(DeltaX,beta,alpha,CG_Z,t,b)
+Ixx_wingbox = Ixx_wingbox(DeltaX,beta,alpha,CG_Z,t,t_side,b)
 
 
 
-def Izz_wingbox(DeltaX,beta,alpha,CG_X,t,b):
+def Izz_wingbox(DeltaX,beta,alpha,CG_X,t,t_side,b):
 
     Izz1 = ((t*((DeltaX)**3)*((cos(beta))**2))/(12*((cos(beta))**3)))+(t*(DeltaX)/cos(beta))*(CG_X-(DeltaX/2))**2    #lower left angled profile
 
     Izz2 = ((t*(DeltaX)**3)*((cos((alpha))**2))/(12*((cos(alpha))**3)))+(t*DeltaX/(cos(alpha)))*(CG_X-(DeltaX/2))**2   #upper right angled profile
 
-    Izz3 = t*(DeltaX * tan(beta) + b + DeltaX * tan(alpha))*((CG_X)**2)  #vertical profile on the left
+    Izz3 = t_side*(DeltaX * tan(beta) + b + DeltaX * tan(alpha))*((CG_X)**2)  #vertical profile on the left
 
-    Izz4 = t*b*((DeltaX-CG_X)**2)   #vertical profile on the right
+    Izz4 = t_side*b*((DeltaX-CG_X)**2)   #vertical profile on the right
 
     Izz_wingbox = Izz1 + Izz2 + Izz3 + Izz4
 
     return Izz_wingbox
 
-Izz_wingbox = Izz_wingbox(DeltaX,beta,alpha,CG_X,t,b)
+Izz_wingbox = Izz_wingbox(DeltaX,beta,alpha,CG_X,t,t_side,b)
 
 
 
-def Ixz_wingbox(DeltaX,beta,alpha,CG_X,CG_Z,t,b): 
+def Ixz_wingbox(DeltaX,beta,alpha,CG_X,CG_Z,t,t_side,b): 
 
 
     Ixz1 = (1/12) * t * (DeltaX/cos(beta))**3 * sin(beta) * cos(beta) + (DeltaX/cos(beta)) * t * ((DeltaX/2)-CG_X) * (-CG_Z-((DeltaX * tan(beta))/2)) #lower left angled profile (+x)*(+z)
 
     Ixz2 = (1/12) * t * (DeltaX/cos(alpha))**3 * sin(alpha) * cos(alpha) + (DeltaX/cos(alpha))*t*((DeltaX/2)-CG_X)*(-b-DeltaX *tan(beta)-((DeltaX*tan(alpha))/2)-CG_Z) #upper right angled profile (+x)*(-z)
 
-    Ixz3 = (DeltaX*tan(beta)+b+DeltaX*tan(alpha))*t*(-CG_X) * (-CG_Z - (DeltaX * tan(alpha) + b + DeltaX * tan (beta))/2)  #vertical profile on the left (-x)*(+z)
+    Ixz3 = (DeltaX*tan(beta)+b+DeltaX*tan(alpha))*t_side*(-CG_X) * (-CG_Z - (DeltaX * tan(alpha) + b + DeltaX * tan (beta))/2)  #vertical profile on the left (-x)*(+z)
 
-    Ixz4 = b * t * (DeltaX - CG_X) * (-CG_Z -((b+DeltaX * tan(beta))/2))  #vertical profile on the right (+x)*(+z)
+    Ixz4 = b * t_side * (DeltaX - CG_X) * (-CG_Z -((b+DeltaX * tan(beta))/2))  #vertical profile on the right (+x)*(+z)
 
     Ixz_wingbox = Ixz1 + Ixz2 + Ixz3 + Ixz4
 
     return Ixz_wingbox
 
-Ixz_wingbox = Ixz_wingbox(DeltaX,beta,alpha,CG_X,CG_Z,t,b)
+Ixz_wingbox = Ixz_wingbox(DeltaX,beta,alpha,CG_X,CG_Z,t_side,t,b)
     
     
 ############################################################################################################################################################################################
