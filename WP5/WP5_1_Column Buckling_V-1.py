185b0994502a0ee310fa1c590ba988fe7d14cb9c
nachos-dighe <ndighe2002@gmail.com>
2021-12-19 17:50:09 +0100

    col buckling updated

--- WP5/WP5_1_Column Buckling_V-1.py
+++ WP5/WP5_1_Column Buckling_V-1.py
@@ -34,6 +34,26 @@ z_TE_top = z_LE-z_LE_bottom-DeltaX*np.tan(alpha)
 t_wingbox_tb = 0.00198 #as checked last time
 t_wingbox_side = 0.004 
 
+#initialisation: wingbox (all design options)
+A_stringer_L = 0.000175  #assumed [m]
+A_stringer_I = 0.000275  #assumed [m]
+#A_stringer_root_bottom = 0.0003 #assumed [m]
+#A_stringer_tip_bottom = 0.0003 #assumed [m]
+
+t_stringer_top = 0.001 #assumed [m] #const thickness
+t_stringer_bottom = 0.001 #assumed [m] #const thickness
+
+#initialisation (stringer instances)
+n_stringer_top = 4#np.array([4, 3,2]) #can be changed
+n_stringer_bottom = 3# np.array([3, 2,2]) #can be changed
+type_stringer_lst_top = np.array([1,0,0,1])#np.array([[1,0,0,1], [1,0,1], [1,1]]) #0: I-stringer; 1: L-stringer (come up with better implementation)
+type_stringer_lst_bottom = np.array([1,0,1])#np.array([[1,0,1], [1,1], [1,1]]) #0: I-stringer; 1: L-stringer (come up with better implementation)
+
+#optimisation
+#A_tot = A_stringer_root_top*n_stringer_top+A_stringer_root_bottom*n_stringer_bottom
+A_tot = 4*A_stringer_L + (n_stringer_top+n_stringer_bottom-4)*A_stringer_I
+
+
 #MOI without stringers
 def MOI_calculator(DeltaX, beta, alpha, z, b):
     Ixx1_per_t = ((((DeltaX) ** 3) * ((np.sin(beta)) ** 2)) / (12 * ((np.cos(beta)) ** 3))) + ((DeltaX) / np.cos(beta)) * (
@@ -52,12 +72,12 @@ def MOI_calculator(DeltaX, beta, alpha, z, b):
     Ixx = (Ixx1_per_t + Ixx2_per_t)*t_wingbox_side + (Ixx3_per_t + Ixx4_per_t)*t_wingbox_tb
     return Ixx
 
-def column_buckling(z_LE_0, z_TE_0, I_wb, BM, n_stringer, type_stringer_lst, Astringer_lst, t):  #z_LE_0,z_TE_0  (0: can be replaced with top/bottom), BM: pos/neg crit,
+def column_buckling(z_LE_0, z_TE_0, I_wb, BM, n_stringer, type_stringer_lst, t):  #z_LE_0,z_TE_0  (0: can be replaced with top/bottom), BM: pos/neg crit,
  
     #stringer material/geometry
     E = 68.9 * 10 ** 9
-    L = 24.63 / 2  # half-span (w.o. coordinate transformation)
-    K = 1 / 4  # K= (1/4) one fixed, one free end  #K = 4 one fixed, one free end
+    L = 5.5  #1.375#5.5 #24.63 / 2  # half-span (w.o. coordinate transformation)
+    K = 4 #1 / 4  # K= (1/4) one fixed, one free end  #K = 4 one fixed, one free end
     #sigma_yield = 241 * 10 ** 6  # compressive strengh of Aluminum, incorrect approach (refine later)
     Sf = 1.5 #safety factor
 
@@ -70,9 +90,9 @@ def column_buckling(z_LE_0, z_TE_0, I_wb, BM, n_stringer, type_stringer_lst, Ast
     #hard-code
     for i in range(0, n_stringer):
         if i ==0 or i ==(n_stringer-1):
-            Astringer_lst =np.full((len(BM)), 0.000175)
+            Astringer_lst =np.full((len(BM)), A_stringer_L)
         else:
-            Astringer_lst =np.full((len(BM)), 0.000275)
+            Astringer_lst =np.full((len(BM)), A_stringer_I)
         
         if type_stringer_lst[i] ==0: #I-stringer
             alst = (Astringer_lst/t)/4.1 #based on optimised design
@@ -88,9 +108,9 @@ def column_buckling(z_LE_0, z_TE_0, I_wb, BM, n_stringer, type_stringer_lst, Ast
         
         Iminlst *= t #going from [m^3] to [m^4] by including thickness
         #addition of moment of inertia
-        print('Iwb before:  ',I_wb[0])
+        #print('Iwb before:  ',I_wb[0])  #testing #works 
         I_wb += Iminlst + zlst[i]**2*Astringer_lst
-        print('Iwb after:  ',I_wb[0])     
+        #print('Iwb after:  ',I_wb[0])     #testing #works  
         #failure stress
         sigma_crit = (K*np.pi**2*E*Iminlst)/(L**2*Astringer_lst)
         sigma_crit_lst[i, :] = sigma_crit
@@ -115,54 +135,39 @@ def column_buckling(z_LE_0, z_TE_0, I_wb, BM, n_stringer, type_stringer_lst, Ast
     for k in range(0, n_stringer):
         MOS_lst = sigma_crit_lst[k, :]/sigma_allow
         #--------BAD CODE---------#
-        MOS_lst *= 100 #for reasons I can't debug, 
+        ######MOS_lst *= 100 #for reasons I can't debug, 
         #-----END OF BAD CODE -------#
         MOS_lst_lst[k, :] = MOS_lst
     #margin of safety for all stringers, spanwise
     #MOS_lst_lst[i, :] = MOS_lst #no longer needed (if doesnt work, add a for loop)
     
     return (MOS_lst_lst, sigma_crit_lst, sigma_allow)
-
-
-#initialisation: wingbox (all design options)
-A_stringer_root_top = 0.0003  #assumed [m]
-A_stringer_tip_top = 0.0003  #assumed [m]
-A_stringer_root_bottom = 0.0003 #assumed [m]
-A_stringer_tip_bottom = 0.0003 #assumed [m]
-t_stringer_top = 0.001 #assumed [m] #const thickness
-t_stringer_bottom = 0.001 #assumed [m] #const thickness
-
-#initialisation (stringer instances)
-n_stringer_top = 4#np.array([4, 3,2]) #can be changed
-n_stringer_bottom = 3# np.array([3, 2,2]) #can be changed
-type_stringer_lst_top = np.array([1,0,0,1])#np.array([[1,0,0,1], [1,0,1], [1,1]]) #0: I-stringer; 1: L-stringer (come up with better implementation)
-type_stringer_lst_bottom = np.array([1,0,1])#np.array([[1,0,1], [1,1], [1,1]]) #0: I-stringer; 1: L-stringer (come up with better implementation)
-
-#optimisation
-A_tot = A_stringer_root_top*n_stringer_top+A_stringer_root_bottom*n_stringer_bottom
-
 #calculate MOI of WB (w/o stringer)
 I_wb = MOI_calculator(DeltaX, beta, alpha, z_LE_bottom, z_TE) #works
-print('INIT IWB:',I_wb[0])
+#print('INIT IWB:',I_wb[0]) #testing #works 
 
 #geometric computation: wingbox (all design options)
+#no longer needed as stringer area is constant
+'''
 Astringer_top_lst=np.linspace(A_stringer_root_top, A_stringer_tip_top,len(ylst_0_mod)) #works
 Astringer_bottom_lst=np.linspace(A_stringer_root_bottom, A_stringer_tip_bottom,len(ylst_0_mod))#works
-
+'''
 
 #print(n_stringer_top[j], type_stringer_lst_top[j], Astringer_top_lst[j], t_stringer_top[j]) #works
 #spanwise margin of safety
-MOS_lst_lst_top, sigma_crit_top_lst, sigma_allow_top_lst = column_buckling(z_LE_top, z_TE_top, I_wb, BMres_poscrit_mod, n_stringer_top, type_stringer_lst_top, Astringer_top_lst, t_stringer_top) #does not work
-MOS_lst_lst_bottom, sigma_crit_bottom_lst, sigma_allow_bottom_lst  = column_buckling(z_LE_bottom, z_TE_bottom, I_wb, BMres_negcrit_mod, n_stringer_bottom, type_stringer_lst_bottom, Astringer_bottom_lst, t_stringer_bottom)
+MOS_lst_lst_top, sigma_crit_top_lst, sigma_allow_top_lst = column_buckling(z_LE_top, z_TE_top, I_wb, BMres_poscrit_mod, n_stringer_top, type_stringer_lst_top, t_stringer_top) #does not work
+MOS_lst_lst_bottom, sigma_crit_bottom_lst, sigma_allow_bottom_lst  = column_buckling(z_LE_bottom, z_TE_bottom, I_wb, BMres_negcrit_mod, n_stringer_bottom, type_stringer_lst_bottom, t_stringer_bottom)
 
 #PLOTTING
 # multiple line plots
 fig1, ax1 = plt.subplots()
 fig2, ax2 = plt.subplots()
 
+#plot for some span only 
+lim = 500
 for n in range(0,n_stringer_top):
     label_txt = 'Stringer: ' + str(n+1)
-    ax1.plot(ylst_0_mod,MOS_lst_lst_top[n,:], label = label_txt)
+    ax1.plot(ylst_0_mod[:lim],MOS_lst_lst_top[n,:lim], label = label_txt)
 ax1.legend()
 ax1.set_ylabel(r'MOS [-]')
 ax1.set_xlabel(r'Span [m]')
@@ -172,7 +177,7 @@ ax1.grid(True)
 
 for n in range(0,n_stringer_bottom):
     label_txt = 'Stringer: ' + str(n+1)
-    ax2.plot(ylst_0_mod,MOS_lst_lst_bottom[n,:], label = label_txt)
+    ax2.plot(ylst_0_mod[:lim],MOS_lst_lst_bottom[n,:lim], label = label_txt)
 ax2.legend()
 ax2.set_ylabel(r'MOS [-]')
 ax2.set_xlabel(r'Span [m]')
